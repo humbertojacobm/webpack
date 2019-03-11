4138252fd287470a37abb586d34940252946a194
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-03-11 18:04:33 -0500

    modulo 02 video 09 plugins

---
+++ plugin-extract-text/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: path.resolve(__dirname,'index.js'),
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+      //aqui van los loaders
+      {
+        //test: que tipo de archivo quiero reconocer.
+        //use: que loader se va a encargar del archivo.
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          //["style-loader","css-loader"]
+          //fallback:style-loader
+          use: "css-loader"
+        }),
+      }
+    ]
+  },
+  plugins:[
+    //aqui van los plugins.
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
