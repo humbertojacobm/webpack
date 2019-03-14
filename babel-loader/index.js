b3a252ff22c75c6e4800f06ad4059bd2f9b72dfd
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-03-14 18:29:39 -0500

    module 3 video 12 hold ecmascript

---
+++ babel-loader/index.js
@@ -0,0 +1,5 @@
+import './estilos.css'
+import {firstMessage} from './message.js';
+document.write(firstMessage);
+// console.log('hola mundo desde webpack');
+console.log('hola mundo desde webpack, en un webpack.config.jszzzzz');
