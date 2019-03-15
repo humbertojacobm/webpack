af7e1ce12a352058037ba4469da3b29a52807249
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-03-15 17:58:27 -0500

    module 03 video 15 holding fonts in webpack

---
+++ url-loader-fonts/src/js/index.js
@@ -0,0 +1,17 @@
+import '../css/estilos.css'
+import {firstMessage
+  ,delayedMessage} from './message.js';
+import platziImg from '../images/platzi.png';
+
+document.write(firstMessage);
+delayedMessage();
+
+const img = document.createElement('img');
+img.setAttribute('src',platziImg);
+img.setAttribute('width',50);
+img.setAttribute('height',50);
+
+document.body.append(img);
+
+// console.log('hola mundo desde webpack');
+console.log('hola mundo desde webpack, en un webpack.config.jszzzzz');
