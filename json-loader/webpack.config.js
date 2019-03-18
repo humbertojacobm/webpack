83ee94f2c28d5a1bd870f7317eaf01a332b9e04b
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-03-18 18:41:10 -0500

    module 03 video 17 supporting json files

---
+++ json-loader/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: ['babel-polyfill',path.resolve(__dirname,'src/js/index.js')],
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+      //aqui van los loaders
+      {
+        //test: que tipo de archivo quiero reconocer.
+        //use: que loader se va a encargar del archivo.
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test:/\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test:/\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+             limit: 200000
+          }
+        }
+      },
+      {
+        //test: que tipo de archivo quiero reconocer.
+        //use: que loader se va a encargar del archivo.
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          //["style-loader","css-loader"]
+          //fallback:style-loader
+          use: "css-loader"
+        }),
+      }
+    ]
+  },
+  plugins:[
+    //aqui van los plugins.
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
